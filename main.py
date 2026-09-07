9560c692df6713cc0e9c26125403e49f52136f50
sanaullahhussain07-pixel <sanaullahhussain07@gmail.com>
2026-09-07 09:45:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
+import threading
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run, daemon=True).start()
+
 import time
 import requests
 from bs4 import BeautifulSoup
